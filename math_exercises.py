849edb945157b502229ea2da281193b7cb9f84fb
Kermo Kao <kermo.kao@voco.ee>
2025-09-29 10:52:44 +0300

    Ülesanne: Matemaatilised avaldised

---
+++ math_exercises.py
@@ -0,0 +1,16 @@
+#Sum and difference
+
+num_a = 3
+num_b = 2
+sum = num_a + num_b
+difference = num_a - num_b
+
+#Float division
+
+division = num_a / num_b
+print(division)
+
+#Integer division
+
+division = num_a // num_b
+print(division)
