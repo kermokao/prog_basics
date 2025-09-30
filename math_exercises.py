d19e156a4a9898cd086b683e1f4ad6002d277596
Kermo Kao <kermo.kao@voco.ee>
2025-09-30 08:38:13 +0300

    Ülesanne: Keerukamad matemaatilised avaldised Moodle

--- math_exercises.py
+++ math_exercises.py
@@ -1,30 +1,34 @@
-# time converter
+"""Math exercises vol2."""
 import math
 
-seconds = 4500
-minutes = seconds // 60
-hours = minutes // 60
-minutes = minutes % 60
-print(f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it).")
 
-#student helper
+def time_converter(seconds: int) -> str:
+    """Convert time in seconds to hours and minutes."""
+    minutes = seconds // 60
+    hours = minutes // 60
+    minutes = minutes % 60
+    return f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it)."
 
 
-rad = math.radians(angle)
-sine = math.sin(rad)
-cosine = math.cos(rad)
-sine = round(sine, 1)
-cosine = round(cosine, 1)
-print(cosine)
+def student_helper(angle: int) -> str:
+    """Return the sine and cosine of the given angle in degrees."""
+    rad = math.radians(angle)
+    sine = math.sin(rad)
+    cosine = math.cos(rad)
+    sine = round(sine, 1)
+    cosine = round(cosine, 1)
+    return f"Nurk: {angle}, siinus: {sine}, koosinus: {cosine}."
 
-#greetings
 
-lots_of_heys = ("Hey") * n
+def greetings(n: int) -> str:
+    """Return a string that contains "Hey" n times."""
+    lots_of_heys = ("Hey") * n
+    return lots_of_heys
 
-#adding number
 
-num_a = 1
-num_b = 23
-string_number = str(num_a) + str(num_b)
+def adding_numbers(num_a: int, num_b: int) -> str:
+    """Return given numbers added together as a string."""
+    string_numbers = str(num_a) + str(num_b)
+    return string_numbers
 
 
