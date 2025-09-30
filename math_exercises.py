0778adaac6cc38b973691e5d7976f9ee3952922a
Kermo Kao <kermo.kao@voco.ee>
2025-09-30 08:35:28 +0300

    Ülesanne: Keerukamad matemaatilised avaldised

--- math_exercises.py
+++ math_exercises.py
@@ -1,70 +1,30 @@
-"""Math exercises."""
+# time converter
 import math
 
+seconds = 4500
+minutes = seconds // 60
+hours = minutes // 60
+minutes = minutes % 60
+print(f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it).")
 
-def sum_and_difference(num_a: int, num_b: int) -> tuple:
-    """Return the sum and difference of given variables num_a and num_b."""
-    sum = num_a + num_b
-    difference = num_a - num_b
-    return sum, difference
-
-
-def float_division(num_a: int, num_b: int) -> float:
-    """Divide given variables num_a and num_b and return the result."""
-    division = num_a / num_b
-    return division
+#student helper
 
 
-def integer_division(num_a: int, num_b: int) -> int:
-    """Divide given variables num_a and num_b and return the result rounded down."""
-    division = num_a // num_b
-    return division
+rad = math.radians(angle)
+sine = math.sin(rad)
+cosine = math.cos(rad)
+sine = round(sine, 1)
+cosine = round(cosine, 1)
+print(cosine)
 
+#greetings
 
-def powerful_operations(num_a: int, num_b: int) -> tuple:
-    """Return the product of given variables, num_a to the power of num_b and the remainder of division of variables."""
-    multiply_numbers = num_a * num_b
-    power = num_a ** num_b
-    remainder = num_a % num_b
-    return multiply_numbers, power, remainder
+lots_of_heys = ("Hey") * n
 
+#adding number
 
-def find_average(num_a: int, num_b: int) -> float:
-    """Return the average of given variables."""
-    average = (num_a + num_b) / 2
-    return average
-
-
-def area_of_a_circle(radius: float) -> float:
-    """Calculate and return the area of a circle."""
-    answer = math.pi * (radius * radius)
-    circle_area = round(answer, 2)
-    return circle_area
-
-
-def area_of_an_equilateral_triangle(side_length: float) -> int:
-    """Calculate and return the area of an equilateral triangle."""
-    triangle_area = math.sqrt(3) / 4 * side_length ** 2
-    triangle_area = round(triangle_area)
-    return triangle_area
-
-
-def calculate_discriminant(a: int, b: int, c: int) -> int:
-    """Calculate discriminant with given variables and return the result."""
-    discriminant = b ** 2 - 4 * a * c 
-    return discriminant
-
-
-def calculate_hypotenuse_length(a: int, b: int) -> float:
-    """Return the length of hypotenuse when the lengths of the catheti are given."""
-    c = math.sqrt(a ** 2 + b ** 2) 
-    return c
-
-
-def calculate_cathetus_length(a: int, c: int) -> float:
-    """Return the length of cathetus when the lengths of the second cathetus and hypotenuse are given."""
-    b = math.sqrt(c ** 2 - a ** 2)
-    return b
-
+num_a = 1
+num_b = 23
+string_number = str(num_a) + str(num_b)
 
 
