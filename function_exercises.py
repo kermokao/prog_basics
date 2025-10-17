6760a4d21872b1a4e07136e86e77a618bad5db1b
Kermo Kao <kermo.kao@voco.ee>
2025-10-17 09:23:40 +0300

    Ülesanne: for-tsükkel Moodle

--- function_exercises.py
+++ function_exercises.py
@@ -1,29 +1,9 @@
-"""Function examples."""
+def sum_between(start: int, end: int) -> int:
+    total = 0
+    for number in range(start, end + 1):
+        total = total + number
+    return total
+print(sum_between(3,5))
+        
 
-
-def func():
-    """123."""
-    print("I´m inside the function")
-
-
-def my_name_is(name) -> str:
-    """123."""
-    print("My name is " + (name))
-
-
-def sum_six(num) -> int:
-    """123."""
-    return 6 + num
-
-
-def sum_numbers(num_a: int, num_b: int):
-    """123."""
-    return num_a + num_b
-
-
-def usd_to_eur(usd: int):
-    """123."""
-    return usd * 0.8
-    
-    
-    
+        
