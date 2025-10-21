d29d26a39dc9915c66d9c446cfd02e738c04a710
Kermo Kao <kermo.kao@voco.ee>
2025-10-21 11:06:52 +0300

    Ülesanne Sõne

---
+++ string_exercises.py
@@ -0,0 +1,17 @@
+first_name = "James"
+last_name = "Bond"
+
+full_name = first_name + last_name
+self_description_name = "f My name is {last_name},{first_name}{last_name}"
+
+cake = "vahukoor\nmarjad\ntäidis\npõhi"
+#print(cake)
+
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[9::-1]
+
+print(backwards)
+print(every_other)
+print(first_word_reversed)
