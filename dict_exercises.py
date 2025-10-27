72c8c4a842d0f6831596283725227e1dc68e157c
Kermo Kao <kermo.kao@voco.ee>
2025-10-27 12:11:14 +0200

    Ülesanne: Sõnastik Moodle

--- dict_exercises.py
+++ dict_exercises.py
@@ -1,23 +1,76 @@
+"""Dictionary exercises."""
+
+
 def get_hobbies(hobbies_dict: dict, name: str) -> list:
+    """
+    Return the hobbies of a given person.
+
+    hobbies = {
+    "Tom": ["running", "reading"],
+    "John": ["movies", "music", "swimming"]
+    }
+
+    get_hobbies(hobbies, "Tom")  => ["running", "reading"]
+    get_hobbies(hobbies, "Timmy")  => "name not in dictionary"
+
+    :param hobbies_dict: dictionary with peoples' hobbies.
+    :param name: name of person whose hobbies are to be returned.
+
+    :return: List of hobbies of the person with given name or "name not in dictionary".
+    """
     if name not in hobbies_dict:
         return "name not in dictionary"
     else:
         return hobbies_dict[name]
-    
-get_hobbies(hobbies_dict, "Tom")
+
 
 def find_tallest(height_dict: dict) -> str:
+    """
+    Return the name of the tallest peron in given dictionary.
+
+    find_tallest({"Tom": 186, "Mari": 175, "Jhon": 190}) => "Jhon"
+
+    :param height_dict: dictionary with peoples' names and heights
+    :return name of tallest person in given dict.
+    """
     tallest = max(height_dict, key=height_dict.get)
     return tallest
 
-def remove_sixes(six_dict: dict) -> dict:
-    return {keys: value for key, value in six_dict.items() if value % 6 != 0}
-    
-def exchange_keys_and_values(exchange_dict: dict) ->:
-   swap_list = {value: key for key, value in exchange_dict.items()}
-   return swap_list
 
-def count_symbol_appearances(stringy:str) -> dict:
+def remove_sixes(six_dict: dict) -> dict:
+    """
+    Return a dictionary where all keys which's values are dividable by six are removed.
+
+    remove_sixes({"a": 4, "b": 8, "c": 6, "d": 18}) => {"a": 4, "b": 8}
+
+    :param six_dict: dictionary to be modified.
+    :return: dict without values that are dividable by six.
+    """
+    return {key: value for key, value in six_dict.items() if value % 6 != 0}
+
+
+def exchange_keys_and_values(exchange_dict: dict) -> dict:
+    """
+    Return a dict where keys and values have been exchanged.
+
+    exchange_keys_and_values({"a": "b", "c": "d"}) => {"b": "a", "d": "c"}
+
+    :param exchange_dict: dictionary to be modified.
+    :return dictionary where values and keys have been exchanged.
+    """
+    swap_list = {value: key for key, value in exchange_dict.items()}
+    return swap_list
+
+
+def count_symbol_appearances(stringy: str) -> dict:
+    """
+    Return dict where key is the symbol and the value is the count this symbol is present in the string.
+
+    count_symbol_appearances("hello hi") => {'h': 2, 'e': 1, 'l': 2, 'o': 1, ' ': 1, 'i': 1}
+
+    :param stringy: string to be processed.
+    :return: dictionary with symbol counts.
+    """
     result = {}
     for word in stringy:
         letter = word
@@ -27,7 +80,18 @@ def count_symbol_appearances(stringy:str) -> dict:
             result[letter] += 1
     return result
 
+
 def organise_by_first_symbol(strings: list) -> dict:
+    """
+    Return dict where key the is a symbol and the value is a list of words starting with this symbol.
+
+    organise_by_first_symbol(["hello", "word", "world", "welcome", "yes"]) =>
+        {'h': ['hello'], 'w': ['word', 'world', 'welcome'], 'y': ['yes']}
+
+    :param strings: list of strings.
+    :return: dict with starting symbol and corresponding words in order of appearance.
+    """
+    # your code goes here
     result = {}
     for word in strings:
         first_symbol = word[0]
@@ -36,7 +100,3 @@ def organise_by_first_symbol(strings: list) -> dict:
         else:
             result[first_symbol].append(word)
     return result
-
-print(count_symbol_appearances("hello hi"))
-print(organise_by_first_symbol(["hello", "word", "world", "welcome", "yes"]))
-
